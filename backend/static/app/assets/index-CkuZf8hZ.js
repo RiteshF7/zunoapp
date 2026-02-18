b70591e62f70f7ab5f8b9cb102f250197f57257d
riteshf7 <riteshf7@gmail.com>
2026-02-18 15:25:08 +0530

    Build UI for production

--- backend/static/app/assets/index-CkuZf8hZ.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Yw6FeW2v.js","assets/index-bbKxZ7P0.js","assets/index-CUWWTLKb.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as o}from"./index-bbKxZ7P0.js";const _=e("Browser",{web:()=>o(()=>import("./web-Yw6FeW2v.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
---
+++ backend/static/app/assets/index-Uc0pYOPl.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-i5P-la09.js","assets/index-DOukHvrg.js","assets/index-CUWWTLKb.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-DOukHvrg.js";const _=e("Browser",{web:()=>o(()=>import("./web-i5P-la09.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
